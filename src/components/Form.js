fd9ab263f97ad1696bf49ad92022e11952524c18
Kevin <kevinolongo9@gmail.com>
2023-10-26 08:23:27 +0300

    Completed lab

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,65 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
+  const [newsletter, setNewsletter] = useState(false);
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleNewsletterChange(event) {
+    // .checked, not .value!
+    setNewsletter(event.target.checked);
+  }
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
   return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <div>
+        <form>
+          <input
+            type="text"
+            onChange={handleFirstNameChange}
+            value={firstName}
+          />
+          <input type="text" onChange={handleLastNameChange} value={lastName} />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+      <div>
+        <form>
+          <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+          <input
+            type="checkbox"
+            id="newsletter"
+            onChange={handleNewsletterChange}
+            checked={newsletter}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+        <form>
+          <input
+            type="text"
+            onChange={props.handleFirstNameChange}
+            value={props.firstName}
+          />
+          <input
+            type="text"
+            onChange={props.handleLastNameChange}
+            value={props.lastName}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
   );
 }
 
